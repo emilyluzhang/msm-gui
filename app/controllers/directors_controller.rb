9ed0bceb915d5bbcb698912a938efd983ea7eb05
emilyluzhang <55498035+emilyluzhang@users.noreply.github.com>
2024-05-11 20:44:07 +0000

    movies done

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,26 @@
 class DirectorsController < ApplicationController
+  def create 
+    # Retrieve useres inputs from params 
+    # "the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"
+
+    m = Movie.new 
+    m.title = params.fetch("the_title")
+    m.year = params.fetch("the_year")
+    m.duration = params.fetch("the_duration")
+    m.description = params.fetch("the_description")
+    m.title = params.fetch("the_image")
+    m.director_id = params.fetch("the_director_id")
+
+    m.save
+
+    redirect_to("/movies")
+
+    # Create a record in the movie table 
+    # Populate each column with the user input 
+    # Save 
+
+    # Redirected user back to the /movies URL
+  end 
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,4 @@
 class MoviesController < ApplicationController
-  def index
-    matching_movies = Movie.all
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
-
-    render({ :template => "movie_templates/index" })
-  end
-
-  def show
-    the_id = params.fetch("path_id")
-
-    matching_movies = Movie.where({ :id => the_id })
-    @the_movie = matching_movies.at(0)
-
-    render({ :template => "movie_templates/show" })
-  end
-
   def create 
     # Retrieve useres inputs from params 
     # "the_title"=>"1", "the_year"=>"2", "the_duration"=>"3", "the_description"=>"4", "the_image"=>"5", "the_director_id"=>"6"
@@ -37,4 +21,47 @@ class MoviesController < ApplicationController
 
     # Redirected user back to the /movies URL
   end 
+  def update 
+    m_id = params.fetch("the_id")
+    matching_records = Movie.where({:id => m_id})
+    @the_movie = matching_records.at(0)
+    @the_movie.title = params.fetch("the_title")
+    @the_movie.year = params.fetch("the_year")
+    @the_movie.duration = params.fetch("the_duration")
+    @the_movie.description = params.fetch("the_description")
+    @the_movie.title = params.fetch("the_image")
+    @the_movie.director_id = params.fetch("the_director_id")
+
+    @the_movie.save
+
+    redirect_to("/movies/#{m_id}")
+  end 
+  def destroy 
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({:id => the_id})
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+    redirect_to("/movies")
+
+
+  end 
+  def index
+    matching_movies = Movie.all
+    @list_of_movies = matching_movies.order({ :created_at => :desc })
+
+    render({ :template => "movie_templates/index" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_movies = Movie.where({ :id => the_id })
+    @the_movie = matching_movies.at(0)
+
+    render({ :template => "movie_templates/show" })
+  end
+
 end
